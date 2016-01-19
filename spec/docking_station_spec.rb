3e0d1a83c5325db03a51c0fd87adead9d0b62a2a
Mic Cassano <mic.cassano@gmail.com>
2016-01-19 17:08:28 +0000

    Add failing spec test for docking bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,11 @@ describe DockingStation do
   it 'returns a working Bike' do
     expect(subject.release_bike).to be_working
   end
+
+  it 'docks a bike' do
+    station = DockingStation.new
+    bike = Bike.new
+    allow(station).to receive(:dock_bike).and_return(bike)
+    expect(station.bike).to eq bike
+  end
 end
