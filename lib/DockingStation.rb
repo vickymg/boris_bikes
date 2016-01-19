3b97f4023ab709cc9446b2c45ee7250a8ea16402
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-19 17:39:48 +0000

    updated DockingStation to work correctly

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,3 +1,50 @@
-class DockingStation 
+require_relative 'Bike.rb'
 
+class DockingStation 
+	attr_accessor :bike
+	# @bikes_in_docks = []
+	def initialize
+	   @bikes_in_dock = []
+	end
+
+	def release_bike 
+		# @new_bike = 
+		Bike.new
+	end
+
+	def dock(bike)
+		@bike = bike
+		@bikes_in_dock << bike
+	end
+
+	def view_dock
+		@bikes_in_dock
+	end
 end
+
+station = DockingStation.new
+
+bike1 = station.release_bike
+
+station.dock(bike1)
+
+bike2 = station.release_bike
+
+station.dock(bike2)
+
+puts station.view_dock
+
+bike3 = station.release_bike
+
+station.dock(bike3)
+
+bike4 = station.release_bike
+
+station.dock(bike4)
+
+puts
+
+puts
+
+puts station.view_dock
+
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,26 @@
 require 'DockingStation'
+#require 'Bike'
 
 describe DockingStation do
+	subject(:DockingStation) {described_class.new}
+  	
+  	it { is_expected.to respond_to :release_bike }
 
+  	it 'expects bike to be working in release_bike' do
+  		expect(subject.release_bike.working?).to be true
+  	end
+
+  	it 'returns docked bikes' do
+  		bike = Bike.new
+  		subject.dock(bike)
+  		expect(subject.bike).to eq bike
+  	end
+  	# it 'expects DockingStation.view_dock to show @bikes_in_dock' do
+  	# 	expect(subject.view_dock).to eq @bikes_in_docks
+  	# end
+
+  	it { is_expected.to respond_to(:dock).with(1).argument }
+  	# it 'expects bikes_in_dock to include new bike' do
+  	# 	expect(subject(bikes_in_dock)).to eq
+  	# end
 end
