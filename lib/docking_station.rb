177c0083bfbb13a922a48e1631b8825dee324a85
Mic Cassano <mic.cassano@gmail.com>
2016-01-19 18:14:32 +0000

    Refactor docking_station_spec and docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
 
 	def initialize
-		@docked_bike = nil
+		@bike = nil
 	end
-
-	attr_reader :docked_bike
 
   def release_bike
     Bike.new
   end
 
-  def dock_bike(bike)
-  	@docked_bike = bike
+  def dock(bike)
+  	@bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,21 @@
 require "docking_station"
 
 describe DockingStation do
-	test_station = DockingStation.new
-  test_bike = Bike.new
-  test_station.dock_bike(test_bike)
-
   it { is_expected.to respond_to :release_bike }
-
   it 'returns a working Bike' do
     expect(subject.release_bike).to be_working
   end
 
-  it {is_expected.to respond_to :dock_bike}
-
+  it {is_expected.to respond_to :dock}
   it 'docks a bike' do
-    expect(test_station.docked_bike).to eq test_bike
+    test_bike = Bike.new
+    expect(subject.dock(test_bike)).to eq test_bike
+  end
+
+  it {is_expected.to respond_to :bike}
+  it 'returns docked bike' do
+    test_bike = Bike.new
+    subject.dock(test_bike)
+    expect(subject.bike).to eq test_bike
   end
 end
