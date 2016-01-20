c117ae0671400b51d3e087a29d8b8e5303ccaad6
seanhawkridge <sean@hawkridge.co>
2016-01-20 11:32:39 +0000

    Challenge 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,21 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to :bike }
+
+  it 'checks if the bike has been docked' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
 end
