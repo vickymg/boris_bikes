1d512791735ff7974bb4a26750904a5ea97bc7f9
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-01-20 10:38:30 +0000

    10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,8 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_accessor :bike
-
-  def initialize
-    @bike = []
-  end
-
   def release_bike
-    raise "No bikes available" if @bike.empty?
-    @bike.pop
-  end
-
-  def dock(bike)
-    raise "Docking station full" if @bike.length >= 20
-    @bike << bike
+    Bike.new
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,34 +3,13 @@ require 'bike'
 
 describe DockingStation do
 
-    # expects instances of DockingStation to respond to a method dock,
-    # with one argument passed in
-    it { is_expected.to respond_to(:dock).with(1).argument }
-
-
-    # Call 'dock' method on instances of DockingStation, and
-    # pass argument as variable 'bike' (creating new instance of Bike)
-    # expect instance variable bike to equal to argument bike
   describe '#release_bike' do
-      # it 'releases a bike' do
-      #   bike = Bike.new
-      #   subject.dock(bike)
-      #   expect(subject.bike).to eq bike
-      # end
-
-    # expect an error to be raised when release_bike method is called on
-    # instances of DockingStation
-      it "raises an error when there are no bikes available" do
-        expect {subject.release_bike}.to raise_error("No bikes available")
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bike).to eq bike
       end
-    end
 
-    describe '#dock' do
-      it "raises an error when the docking station is at capacity" do
-        20.times {subject.dock Bike.new}
-        expect { subject.dock Bike.new }.to raise_error("Docking station full")
-      end
-    end
     #instances of DockingStation should respond to release_bike method
     it { should respond_to :release_bike }
 
