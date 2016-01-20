4f8d1330d254b07472b4b377282a10886fcaad87
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-01-20 10:14:56 +0000

    to 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_accessor :bike
+
+  def initialize
+    @bike = []
+  end
+
   def release_bike
-    Bike.new
+    raise "No bikes available" if @bike.empty?
+    @bike.pop
+  end
+
+  def dock(bike)
+    raise "Docking station full" if @bike.length >= 20
+    @bike << bike
   end
 
 end
