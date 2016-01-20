6f33947dc44861e79c0e1aa9d549a7a3b7a3f692
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-01-20 10:43:13 +0000

    rspec working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,10 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 
-  describe '#release_bike' do
-      it 'releases a bike' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
-      end
-
-    #instances of DockingStation should respond to release_bike method
-    it { should respond_to :release_bike }
-
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
