bb4d3ca2bb68aa50ca11eed7bf0cc79c78b3a698
Mic Cassano <mic.cassano@gmail.com>
2016-01-21 15:28:32 +0000

    Implement Van#collect_bikes and respectives tests

--- lib/van.rb
+++ lib/van.rb
@@ -2,11 +2,20 @@
 require_relative 'docking_station'
 
 class Van
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def collect_bikes(station)
-    station.bikes.select do |bike|
+    @bikes = station.bikes.select do |bike|
       !bike.working
     end
+  end
+
+  def deliver_bikes(garage)
+
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,13 +2,32 @@ require 'van'
 
 describe Van do
 
-  it {is_expected.to respond_to(:collect_bikes).with(1).argument}
+  describe '#collect_bikes' do
+    it {is_expected.to respond_to(:collect_bikes).with(1).argument}
 
-  let(:broken_bike) {double(:bike, :working => false)}
-  let(:working_bike) {double(:bike, :working => true)}
-  let(:station) {double(:docking_station, :bikes => [broken_bike, working_bike])}
-  it 'allows vans to collect bikes' do
-    expect(subject.collect_bikes(station)).to eq [broken_bike]
+    let(:broken_bike) {double(:bike, :working => false)}
+    let(:working_bike) {double(:bike, :working => true)}
+    let(:station) {double(:docking_station, :bikes => [broken_bike, working_bike])}
+
+    it 'allows vans to collect bikes' do
+      subject.collect_bikes(station)
+      expect(subject.bikes).to eq [broken_bike]
+    end
+  end
+
+  describe '#deliver_bikes' do
+    it {is_expected.to respond_to(:deliver_bikes).with(1).argument}
+
+    let(:broken_bike1) { double(:bike, :working => false) }
+    let(:broken_bike2) { double(:bike, :working => false) }
+    let(:station) {double(:docking_station, :bikes => [broken_bike1, broken_bike2])}
+    let(:garage) { double :garage }
+
+    it 'delivers broken bikes to a garage' do
+      subject.collect_bikes(station)
+      subject.deliver_bikes(garage)
+      expect(subject.bikes).to be_empty
+    end
   end
 
 end
