95ffb34ed2527247f1d17fc58fc84097adb2cf7c
Vicky Gray <vickygray26@hotmail.com>
2016-06-20 13:14:02 +0100

    Docking station's release_bike method returns a working bike - bike class' working? method returns true

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
